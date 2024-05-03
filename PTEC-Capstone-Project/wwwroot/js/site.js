b29be2f993dc0056ac7e3bb3f22fd231e3eb6a0b
Hamza Haque <hamzahaque30@gmail.com>
2024-05-03 11:31:17 -0500

    Add JS toggle for mobile menu

--- PTEC-Capstone-Project/wwwroot/js/site.js
+++ PTEC-Capstone-Project/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function menuClick() {
+  menuBtn.classList.add("opened-menu");
+  mobileMenu.classList.add("show-menu");
+}
+
+var menuBtn = document.getElementById("menu-btn");
+var mobileMenu = document.getElementById("mobile-menu");
+
+menuBtn.addEventListener("click", menuClick);
