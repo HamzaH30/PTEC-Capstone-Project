c9b3b8dd9b4e35ba9f501be8ccab87c6fbd5923c
Hamza Haque <hamzahaque30@gmail.com>
2024-05-08 10:56:37 -0500

    Add CreatePost markup

--- PTEC-Capstone-Project/Controllers/HomeController.cs
+++ PTEC-Capstone-Project/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace PTEC_Capstone_Project.Controllers
             return View();
         }
 
+        public IActionResult CreatePost()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
