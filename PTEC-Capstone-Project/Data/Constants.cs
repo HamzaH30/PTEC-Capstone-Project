2e334f78a7f69ec0a9c33ca73a36ba2d0a84da8e
Hamza Haque <hamzahaque30@gmail.com>
2024-05-10 11:16:08 -0500

    Create Admin & User constant roles

--- PTEC-Capstone-Project/Data/Constants.cs
+++ PTEC-Capstone-Project/Data/Constants.cs
@@ -3,5 +3,7 @@
     public static class Constants
     {
         public const string SuperAdminRole = "Super Admin";
+        public const string AdminRole = "Admin";
+        public const string UserRole = "User";
     }
 }
