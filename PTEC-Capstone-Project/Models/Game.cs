3843298235f6a5a8310014470f059bec5cf66ac9
Hamza Haque <hamzahaque30@gmail.com>
2024-05-14 09:27:19 -0500

    Make some of the props nullable

--- PTEC-Capstone-Project/Models/Game.cs
+++ PTEC-Capstone-Project/Models/Game.cs
@@ -8,9 +8,9 @@ namespace PTEC_Capstone_Project.Models
         [Key] 
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Genre { get; set; }
-        public string Description { get; set; }
-        public string ImageFilePath {  get; set; }
+        public string? Genre { get; set; }
+        public string? Description { get; set; }
+        public string? ImageFilePath {  get; set; }
 
         // Navigation Properties
         public virtual ICollection<Tournament>? Tournaments { get; set; }
