cc0ed446cef2fe2b0006dad7bc6d93015ba25289
Anuj Saul <saul.anuj@gmail.com>
2024-05-19 16:21:27 -0500

    adding view model

---
+++ PTEC-Capstone-Project/Models/ViewModels/GamePostViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PTEC_Capstone_Project.Models.ViewModels
+{
+    public class GamePostViewModel
+    {
+        public string GameName { get; set; }
+        public string UserName {  get; set; }
+        public DateTime TimePosted { get; set; }
+        public string PostDescription { get; set; }
+    }
+}
