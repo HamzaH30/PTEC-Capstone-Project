3caa069212561308f3810cd21d8ee4854ad41e5d
Anuj Saul <saul.anuj@gmail.com>
2024-05-19 20:39:46 -0500

    create view model for creating posts

---
+++ PTEC-Capstone-Project/Models/ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PTEC_Capstone_Project.Models.ViewModels
+{
+    public class CreatePostViewModel
+    {
+        public int GameID { get; set; }
+        public string PostDescription { get; set; }
+        public int NumberOfPlayers { get; set; }
+        public string RankRequired { get; set; }
+        public string LevelRequired { get; set; }
+    }
+}
