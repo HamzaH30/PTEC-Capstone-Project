26b76233c457ec2e5e8b477950e02dc878057cd0
Hamza Haque <hamzahaque30@gmail.com>
2024-05-23 09:48:32 -0500

    Create a services folder & install the Newtonsoft.Json package

--- PTEC-Capstone-Project/Controllers/GamesController.cs
+++ PTEC-Capstone-Project/Controllers/GamesController.cs
@@ -6,6 +6,10 @@ namespace PTEC_Capstone_Project.Controllers
     {
         public IActionResult Index()
         {
+            // Store an API Endpoint
+            // Get the game data from the API
+            // Store the data in the database
+            // Make a view model and pass the game data to the view
             return View();
         }
     }
