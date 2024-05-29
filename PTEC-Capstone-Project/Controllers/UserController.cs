b00f2bbb17ade621dca06b8779ad7f5134d64ebb
Anuj Saul <saul.anuj@gmail.com>
2024-05-29 09:27:54 -0500

    creating controller

---
+++ PTEC-Capstone-Project/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PTEC_Capstone_Project.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
