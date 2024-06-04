31be207d1d4de18f5e790543a744cba586e494ad
Anuj Saul <saul.anuj@gmail.com>
2024-06-04 09:13:10 -0500

    Making controller to handle creating requests

---
+++ PTEC-Capstone-Project/Controllers/RequestController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTEC_Capstone_Project.Data;
+using PTEC_Capstone_Project.Models;
+
+namespace PTEC_Capstone_Project.Controllers
+{
+    public class RequestController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public RequestController(ApplicationDbContext context) 
+        { 
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRequest(int postID)
+        {
+            // find post
+            Post post = _context.Posts.Where(p => p.Id == postID).FirstOrDefault()!;
+
+            // create/find status model
+            
+
+            // create request model
+            Request req = new Request
+            {
+                Timestamp = DateTime.Now,
+                PostID = postID,
+                Post = post,
+
+            };
+            // sync database
+
+
+
+
+            return View();
+        }
+
+        public void CreateOrFindStatus(Statuses status)
+        {
+            
+        }
+    }
+}
--- PTEC-Capstone-Project/Models/RequestStatus.cs
+++ PTEC-Capstone-Project/Models/RequestStatus.cs
@@ -9,4 +9,11 @@ namespace PTEC_Capstone_Project.Models
         public int Id { get; set; }
         public string Description { get; set; }
     }
+
+    public enum Statuses
+    {
+        Pending = 0,
+        Accepted = 1,
+        Denied = 2
+    }
 }
