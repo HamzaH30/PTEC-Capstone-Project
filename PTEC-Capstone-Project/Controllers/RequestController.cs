14b8900485edc0743a1a290a29691fcf62e78b8a
Anuj Saul <saul.anuj@gmail.com>
2024-06-04 09:25:58 -0500

    Logic for creating request and request status

--- PTEC-Capstone-Project/Controllers/RequestController.cs
+++ PTEC-Capstone-Project/Controllers/RequestController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
 using PTEC_Capstone_Project.Data;
 using PTEC_Capstone_Project.Models;
 
@@ -22,7 +23,7 @@ namespace PTEC_Capstone_Project.Controllers
             Post post = _context.Posts.Where(p => p.Id == postID).FirstOrDefault()!;
 
             // create/find status model
-            
+            RequestStatus reqSts = CreateOrFindStatus(Statuses.Pending);
 
             // create request model
             Request req = new Request
@@ -30,23 +31,36 @@ namespace PTEC_Capstone_Project.Controllers
                 Timestamp = DateTime.Now,
                 PostID = postID,
                 Post = post,
-
+                StatusID = reqSts.Id,
+                RequestStatus = reqSts
             };
+
             // sync database
-
-
-
+            _context.Requests.Add(req);
+            _context.SaveChanges();
 
             return View();
         }
 
-        public void CreateOrFindStatus(Statuses status)
+        public RequestStatus CreateOrFindStatus(Statuses status)
         {
             // does the status exist
-            //_context.RequestStatuses.Where(rs => rs.Description == status)
+            RequestStatus? reqSts = _context.RequestStatuses.Where(rs => rs.Name == status).FirstOrDefault();
 
-            // if not create status
+            // if not create status\
+            if (reqSts == null) 
+            {
+                reqSts = new RequestStatus
+                {
+                    Name = status
+                };
+
+                _context.RequestStatuses.Add(reqSts);
+                _context.SaveChanges();
+            }
+
             // if it does return 
+            return reqSts;
         }
     }
 }
