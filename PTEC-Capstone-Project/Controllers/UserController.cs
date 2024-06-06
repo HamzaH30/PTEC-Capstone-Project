ecb5a5da9942dc39edb73b6f05b8f195bf6bba7e
Anuj Saul <saul.anuj@gmail.com>
2024-06-06 09:19:31 -0500

    adding context and user manager to user controller

--- PTEC-Capstone-Project/Controllers/UserController.cs
+++ PTEC-Capstone-Project/Controllers/UserController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PTEC_Capstone_Project.Data;
+using PTEC_Capstone_Project.Models;
 
 namespace PTEC_Capstone_Project.Controllers
 {
     public class UserController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
