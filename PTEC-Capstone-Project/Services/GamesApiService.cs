4740688c3f9d0fa50b9e52c938e95c147b05b2d2
SebWatt <sebastienwatt@icloud.com>
2024-06-09 11:19:44 -0700

    Made a more Broad Browser Selection for the games api

--- PTEC-Capstone-Project/Services/GamesApiService.cs
+++ PTEC-Capstone-Project/Services/GamesApiService.cs
@@ -14,7 +14,7 @@ namespace PTEC_Capstone_Project.Services
             _httpClient = client;
             _apiKey = apiKey;
 
-            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0");
+            _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (compatible; GameApp/1.0; +https://ptec-capstone-project.azurewebsites.net/)");
         }
 
         public async Task<GamesApiResponse> GetApiResponse(string url)
